07ecd810af9cee56794a689d3235bc7c670dfef1
KKravtsov14 <kostya.kravtsov.2001@mail.ru>
2020-02-15 10:16:45 +0700

    made main()

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 
 import turtle as t
 import random as r
-colors = ["green", "yellow", "blue", "brown", "red", "orange", "pink", "black"]
-t.speed(0)
 
 def tree(length):
     """
@@ -224,59 +222,182 @@ def dragon(length, num_iteration, turn):
     else:
         t.forward(length)
 
+def main():
+    colors = ["", "green", "yellow", "blue", "brown", "red", "orange", "pink", "black"]
 
-print('''1. Кривая Коха
-2. Снежинка Коха.
-3. Ледяной фрактал (пример 1)
-4. Ледяной фрактал (пример 2)
-5. Снежинка к ледяному фракталу (пример 1)
-6. Снежинка к ледяному фракталу (пример 2)
-7. Кривая Минковского
-8. Кривая Леви
-9. Построение двоичного дерева
-10. Дракон Хартера-Хейтуэя
-11. Ветвь''')
-fractal = int(input('Введите номер для построения фрактала:'))
-if fractal == 1:
-    koch_line(lenght, num_iteration)
-    t.mainloop()
+    print("""    1. Кривая Коха
+    2. Снежинка Коха.
+    3. Ледяной фрактал (пример 1)
+    4. Ледяной фрактал (пример 2)
+    5. Снежинка к ледяному фракталу (пример 1)
+    6. Снежинка к ледяному фракталу (пример 2)
+    7. Кривая Минковского
+    8. Кривая Леви
+    9. Построение двоичного дерева
+    10. Дракон Хартера-Хейтуэя
+    11. Ветвь""")
 
-elif fractal == 2:
-    k_snowflake(lenght, num_iteration)
-    t.mainloop()
+    fractal = int(input("""Введите номер для построения фрактала: """))
 
-elif fractal == 3:
-    ice_fractal_1(lenght, num_iteration)
-    t.mainloop()
+    print("""Хотите сами вводить характеристики фигуры?
+    1. Да
+    2. Нет""")
 
-elif fractal == 4:
-    ice_fractal_2(lenght, num_iteration)
-    t.mainloop()
+    answer = int(input("""Введите номер ответа: """))
 
-elif fractal == 5:
-    i_f_1_snowflake(lenght, num_iteration)
-    t.mainloop()
+    if fractal == 1:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            koch_line(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            koch_line(200, 4)
+            t.mainloop()
 
-elif fractal == 6:
-    i_f_2_snowflake(lenght, num_iteration)
-    t.mainloop()
+    elif fractal == 2:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            k_snowflake(int(input('Введите длину: ')), int(input('Введите количество углов: ')),
+                        int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            k_snowflake(200, 3, 4)
+            t.mainloop()
 
-elif fractal == 7:
-    minkowski_curve(lenght, num_iteration)
-    t.mainloop()
+    elif fractal == 3:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            ice_fractal_1(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            ice_fractal_1(300, 4)
+            t.mainloop()
 
-elif fractal == 8:
-    levy_curve(lenght, num_iteration)
-    t.mainloop()
+    elif fractal == 4:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            ice_fractal_2(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            ice_fractal_2(300, 4)
+            t.mainloop()
 
-elif fractal == 9:
-    tree(lenght, num_iteration)
-    t.mainloop()
+    elif fractal == 5:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            i_f_1_snowflake(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            t.up()
+            t.backward(300)
+            i_f_1_snowflake(300, 4)
+            t.mainloop()
 
-elif fractal == 10:
-    dragon(lenght, num_iteration, turn)
-    t.mainloop()
 
-elif fractal == 11:
-    branch(lenght, num_iteration, turn)
-    t.mainloop()
+    elif fractal == 6:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            i_f_2_snowflake(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            t.up()
+            t.backward(300)
+            i_f_2_snowflake(300, 4)
+            t.mainloop()
+
+
+    elif fractal == 7:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            minkowski_curve(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            minkowski_curve(300, 3)
+            t.mainloop()
+
+    elif fractal == 8:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            levy_curve(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            levy_curve(3000, 8)
+            t.mainloop()
+
+    elif fractal == 9:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            t.left(90)
+            tree(int(input('Введите длину: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            t.left(90)
+            tree(100)
+            t.mainloop()
+
+    elif fractal == 10:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            dragon(int(input('Введите длину: ')), int(input('Введите количество шагов: ')), 1)
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            dragon(3000, 9, 1)
+            t.mainloop()
+
+    elif fractal == 11:
+        if answer == 1:
+            t.speed(0)
+            print('Возможные цвета: 1. green 2. yellow 3. blue 4. brown 5. red 6. orange 7. pink 8. black')
+            t.color(colors[int(input('Выберите номер цвета из представленных: '))])
+            t.left(90)
+            branch(int(input('Введите длину: ')), int(input('Введите количество шагов: ')))
+            t.mainloop()
+        elif answer == 2:
+            t.speed(0)
+            t.color(colors[r.randint(1, 8)])
+            t.left(90)
+            branch(100, 5)
+            t.mainloop()
+
+
+main()
